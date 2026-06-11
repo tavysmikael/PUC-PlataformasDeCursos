cac2762c3f93f9fe486839465553cf7b97d3f785
tavysmikael <tavysmikael@gmail.com>
2026-06-11 00:42:18 -0300

    Fix NavBar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -28,7 +28,7 @@ export default function Navbar() {
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <Link className="navbar-brand fw-bold text-warning" to="/">
-          🎓 EduPlataforma
+          CURSOS TCS
         </Link>
 
         <button
